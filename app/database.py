f68f91e5a1771dfce61c32181d016816f312e92e
LucioFurnari <luciofurnari99@gmail.com>
2026-01-05 16:15:44 -0300

    feat: create file for orm and model

---
+++ app/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get database URL from env
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create database engine (the connection)
+engine =  create_engine(DATABASE_URL)
+
+# Create a session factory (for database transactions)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for all models
+Base = declarative_base()
+
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
