97f6601e6ad0c94fe4104f256ea494fd2071016a
LucioFurnari <luciofurnari99@gmail.com>
2026-01-06 18:36:46 -0300

    feat: update file

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.database import Base
+from app.models.user import User
+from app.models.exercise import Exercise
+from app.models.workout_plan import WorkoutPlan
+from app.models.workout_exercise import WorkoutExercise
+from app.models.scheduled_workout import ScheduledWorkout
