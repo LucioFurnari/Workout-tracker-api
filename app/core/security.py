f22e03bb04c0932b1ebb0288325a84b30bb141ad
LucioFurnari <luciofurnari99@gmail.com>
2026-01-14 21:41:01 -0300

    feat: add function to create token and validate password

--- app/core/security.py
+++ app/core/security.py
@@ -14,3 +14,19 @@ ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
   return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str) -> str:
+  """ Hash a password """
+  return pwd_context.hash(password)
+
+def create_acces_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+  to_encode = data.copy()
+
+  if expires_delta:
+    expire = datetime.utcnow() + expires_delta
+  else:
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+  
+  to_encode.update({"exp": expire})
+  encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+  return encoded_jwt
