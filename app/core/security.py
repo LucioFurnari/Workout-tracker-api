41ba0e84d12a5e1dc4242bb95e27f6b93a6f4374
LucioFurnari <luciofurnari99@gmail.com>
2026-01-24 12:29:19 -0300

    feat: add function to return refresh token

--- app/core/security.py
+++ app/core/security.py
@@ -13,6 +13,7 @@ SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = settings.ALGORITHM
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
+REFRESH_TOKEN_EXPIRES_DAYS = 7
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
   """Verify a password against a hash"""
@@ -36,6 +37,13 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -
       expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
   
   to_encode.update({"exp": expire})
+  encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+  return encoded_jwt
+
+def create_refresh_token(data: dict):
+  to_encode = data.copy()
+  expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRES_DAYS)
+  to_encode.update({"exp": expire, "type": "refresh"})
   encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
   return encoded_jwt
 
