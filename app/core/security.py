8bb6b84ad0bb91bbe57739f077ee3c5f9d07bb06
LucioFurnari <luciofurnari99@gmail.com>
2026-03-03 17:33:00 -0300

    fix: add type propertie for the token in create token function

--- app/core/security.py
+++ app/core/security.py
@@ -36,7 +36,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -
   else:
       expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
   
-  to_encode.update({"exp": expire})
+  to_encode.update({"exp": expire,  "type": "access"})
   encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
   return encoded_jwt
 
